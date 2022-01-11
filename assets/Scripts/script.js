5cff1c9aef4f0f44dd8b3ed82b42d3e0c31246e0
João Pedro de Almeida <joaopedro.alm.c@gmail.com>
2022-01-10 22:11:33 -0300

    estou quase resolvendo o problema do interval

--- assets/Scripts/script.js
+++ assets/Scripts/script.js
@@ -1,24 +1,40 @@
+// só consegui fazer o interval funcionar corretamente, pois utilizei o a varivel com o metodo construtor Date(), dentro do mesmo, pois ele tem que checar o metodo e executa-lo.
+
 const myTime = document.getElementById('Time');
 const timeButtom = document.getElementById('buton');
 const cronoButtom = document.getElementById('crono');
 
+const onIntervals = (func) => {
+	let timePassingOne = setInterval(watch, 1000);
+	let timePassingTwo = setInterval(cronometer, 1000);
+	if (func === watch && func !== cronometer) {
+		clearInterval(timePassingTwo);
+		return (myTime.innerHTML = watch());
+	}
+	if (func === cronometer && func !== watch) {
+		clearInterval(timePassingOne);
+		return (myTime.innerHTML = cronometer());
+	}
+};
 // aqui é a função do meu relógio
 function watch() {
-	// só consegui fazer o interval funcionar corretamente, pois utilizei o a varivel com o metodo construtor Date(), dentro do mesmo, pois ele tem que checar o metodo e executa-lo.
-	setInterval(function () {
-		const time = new Date();
-		let timer = time.toLocaleString('pt-BR', {
-			hour12: false,
-			hour: '2-digit',
-			minute: '2-digit',
-			second: '2-digit',
-		});
-		myTime.innerHTML = timer;
-	}, 1000);
+	const time = new Date();
+	let timer = time.toLocaleString('pt-BR', {
+		hour12: false,
+		hour: '2-digit',
+		minute: '2-digit',
+		second: '2-digit',
+	});
+	return timer;
 }
-const timePassing = setInterval(watch, 1000);
 
 const cronometer = () => {
-	clearInterval(timePassing);
-	myTime.innerHTML = 'carai to aqui';
+	return 'carai to aqui';
 };
+
+// let timePassing;
+// if (!timeButtom.onclick) {
+// setInterval(watch, 1000);
+// } else if (cronoButtom) {
+// 	clearInterval(timePassing);
+// }

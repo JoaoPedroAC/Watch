dc124b3e8ecfee1488833d5e4322995fa0126e0a
João Pedro de Almeida <78094903+JoaoPedroAC@users.noreply.github.com>
2022-01-13 15:04:35 +0000

    cronometro funcionando

--- assets/Scripts/script.js
+++ assets/Scripts/script.js
@@ -1,26 +1,29 @@
 // só consegui fazer o interval funcionar corretamente, pois utilizei o a varivel com o metodo construtor Date(), dentro do mesmo, pois ele tem que checar o metodo e executa-lo.
 
 const myTime = document.getElementById('Time');
-const timeButtom = document.getElementById('buton');
-const cronoButtom = document.getElementById('crono');
+const cronos = document.querySelector('.cronometro');
+// const timeButtom = document.getElementById('buton');
+// const cronoButtom = document.getElementById('crono');
 let timePassingOne;
 let timePassingTwo;
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
 
 // Essa function é para controlar a execução e dos clicks de cada botão
 const onIntervals = (func) => {
 	if (func === watch) {
-    clearInterval(timePassingTwo);     
     timePassingOne = setInterval(watch, 1000);
-    return timePassingOne;
+    return clearInterval(timePassingTwo);
 	}
 	if (func === cronometer) {
-    clearInterval(timePassingOne);
     timePassingTwo = setInterval(cronometer,1000);
-    return timePassingTwo;
+    return clearInterval(timePassingOne);
 	}
 };
 // aqui é a função do meu relógio
 function watch() {
+  cronos.style.display='none';
 	const time = new Date();
 	let timer = time.toLocaleString('pt-BR', {
 		hour12: false,
@@ -28,12 +31,24 @@ function watch() {
 		minute: '2-digit',
 		second: '2-digit',
 	});
-	return myTime.innerHTML = timer;
+	myTime.innerHTML = timer;
 }
 // aqui é a função do meu cronometro
 const cronometer = () => {
-	return myTime.innerHTML = 'carai to aqui';
+  cronos.style.display='flex';
+  if(seconds>58){
+    seconds=0;
+    minutes++;
+  }
+  if(minutes>58){
+    minutes=0;
+    hours++;
+  }
+  seconds++;
+	myTime.innerHTML = `${zeroLeft(hours)}:${zeroLeft(minutes)}:${zeroLeft(seconds)}`;
 };
+// vai colocar 0 na esquerda
+const zeroLeft =(numeric)=> numeric < 10 ? `0${numeric}`: numeric;
 
 // let timePassing;
 // if (!timeButtom.onclick) {
